19a4f73dc2b2c66ef3407f3e06ce718f2e5b6118
Shashank Gupta <27392shashankgupta@gmail.com>
2016-06-08 23:04:15 +0530

    add files (data, model and training added)

---
+++ training.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+
+''' Training module (mini-batch) '''
+
+import numpy as np
+import theano
+from theano import tensor as T
+from model import AutoEncoder
+
+
+class trainAE(object):
+
+    def __init__(self, path, k, lr= 0.1, batch_size=1, loss='bce', n_epochs=100):
+        '''
+        Arguments:
+            path : path to training data
+            k : hidde unit's dimension
+            lr : learning rate
+            batch_size : batch_size for training, currently set to 1
+            loss : loss function (bce, rmse) to train AutoEncoder
+            n_epochs : number of epochs for training
+        '''
+        self.AE = AutoEncoder(path, k)
+        # Definne the autoencoder model
+        self.AE.model()
+        self.epochs = n_epochs
+
+
+    def train(self):
+        T = self.AE.T
+        # COnverting to csr format for indexing
+        T = T.tocsr()
+        nonzero_indices = T.nonzero()
+        for epoch in xrange(self.epochs):
+            print("Running epoch %d"%(epoch))
+            for i in nonzero_indices[0]:
+                # get indices of observed values from the user 'i' 's vector
+                indices = T[i, :].nonzero()[1]
+                indices = indices.reshape(indices.shape[0],)
+                print indices.shape
+                # Get correspoding ratings
+                ratings = T[i, indices].toarray()
+                loss = self.AE.model([indices, ratings])
+                print("Loss at epoch %d is %f"%(epoch, loss))
+
+
+
+if __name__ == "__main__":
+    autoencoder = trainAE('/home/shashank/data/SIEL/trust-aware-reco/Epinions_signed/soc-sign-epinions.txt', 100)
+    autoencoder.train()
+
+
+
+
